980eaa5458a9f51946d8ae048774a6a16e68b771
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2023-06-20 10:50:01 -0300

    completed processes file reading

--- main.c
+++ main.c
@@ -16,37 +16,74 @@ typedef struct _queue_node // processos
     int process_number; /* numero do processo "Processo #<process_number>" */
     int memory_size;    /* quantidade de memoria necessaria para o processo */
     int info_number;    /* numero de informacoes de exec ou io */
-    int exec_time;      /* tempo de execucao */
+    int exec_time[5];   /* array de tempos de execucao ja q pode ter mais um uma operacao de execucao */
     int pid;            /* id do processo */
-    int io_time;        /* tempo de operacao io */
+    int io_time[5];     /* array de tempos de operacao io ja q pode ter mais de uma operacao io */
     struct _queue_node *next;
 } queue_node;
 
 void init_queue(queue *q);
 int is_queue_empty(queue *q);
-void enqueue(queue *q, int process_number, int memory_size, int info_number, int exec_time, int pid, int io_time);
+void enqueue(queue *q, int process_number, int memory_size, int info_number, int exec_time[], int pid, int io_time[]);
 queue_node *dequeue(queue *q);
 void print_queue(queue *q);
 void free_queue(queue *q);
+int count_process_number(queue *q);
 
 int main(void)
 {
     FILE *file_ptr;
     char c;
-    int num_processes, process_number, memory_size, info_number, exec_time, pid, io_time;
+    int num_processes, process_number, memory_size, info_number;
+    int exec_time[5] = {-1, -1, -1, -1, -1}, pid = -1, io_time[5] = {-1, -1, -1, -1, -1}, i;
+    int count_exec = 0, count_io = 0, read_info = 0;
+    queue *ready_queue = (queue *)malloc(sizeof(queue));
+    queue *blocked_queue = (queue *)malloc(sizeof(queue));
+    init_queue(ready_queue);
+    init_queue(blocked_queue);
 
     file_ptr = fopen("processos.txt", "r");
     while ((c = fgetc(file_ptr)) != EOF)
     {
-        if (isdigit(c)) // essa gambiarrazinha é só pra ler o primeiro char
+        if (c == 'P')
         {
-            fseek(file_ptr, -1, SEEK_CUR);
-            fscanf(file_ptr, "%d\n", &num_processes);
-            break;
+            if (fscanf(file_ptr, "rocesso #%d - %dKb\n%d\n", &process_number, &memory_size, &info_number))
+            {
+                // lendo as informacoes do processo
+                while (read_info != info_number)
+                {
+                    if (fscanf(file_ptr, "exec %d\n", &exec_time[count_exec])) // exec
+                    {
+                        read_info += 1;
+                        count_exec += 1;
+                    }
+                    else if (fscanf(file_ptr, "io %d\n", &io_time[count_io])) // io
+                    {
+                        read_info += 1;
+                        count_io += 1;
+                    }
+                }
+                // reset values
+                read_info = 0;
+                count_exec = 0;
+                count_io = 0;
+            }
+            // criando processo na fila de prontos
+            enqueue(ready_queue, process_number, memory_size, info_number, exec_time, pid, io_time);
+
+            // reset values
+            for (i = 0; i < 5; i++)
+            {
+                io_time[i] = -1;
+                exec_time[i] = -1;
+            }
         }
     }
-    // printf("quantidade de processos: %d\nProcesso #%d - %dKb\n", num_processes, process_number, memory_size);
+    print_queue(ready_queue);
     fclose(file_ptr);
+
+    // salvando quantidade de processos
+    num_processes = count_process_number(ready_queue);
 
     return 0;
 }
@@ -55,15 +92,21 @@ void init_queue(queue *q) { q->head = NULL, q->tail = NULL; }
 
 int is_queue_empty(queue *q) { return (q->head == NULL); }
 
-void enqueue(queue *q, int process_number, int memory_size, int info_number, int exec_time, int pid, int io_time)
+void enqueue(queue *q, int process_number, int memory_size, int info_number, int exec_time[], int pid, int io_time[])
 {
     queue_node *new_node = (queue_node *)malloc(sizeof(queue_node));
     new_node->process_number = process_number;
     new_node->memory_size = memory_size;
     new_node->info_number = info_number;
-    new_node->exec_time = exec_time;
+
+    for (int i = 0; i < 5; i++)
+        new_node->exec_time[i] = exec_time[i];
+
     new_node->pid = pid;
-    new_node->io_time = io_time;
+
+    for (int i = 0; i < 5; i++)
+        new_node->io_time[i] = io_time[i];
+
     new_node->next = NULL;
 
     if (is_queue_empty(q))
@@ -101,11 +144,22 @@ void print_queue(queue *q)
 
     while (current != NULL)
     {
-        printf("%d %d %d %d %d %d", current->process_number, current->memory_size, current->info_number, current->exec_time, current->pid, current->io_time);
-        printf(" | ");
+        printf("num=<%d> ", current->process_number);
+        printf("mem=<%d> ", current->memory_size);
+        printf("info=<%d> ", current->info_number);
+        printf("exec=< ");
+        for (int i = 0; i < 5; i++)
+            printf("%d ", current->exec_time[i]);
+        printf("> ");
+        printf("pid=<%d> ", current->pid);
+        printf("io=< ");
+        for (int i = 0; i < 5; i++)
+            printf("%d ", current->io_time[i]);
+        printf("> ");
+        printf("| ");
         current = current->next;
+        printf("\n");
     }
-    printf("\n");
 }
 
 void free_queue(queue *q)
@@ -121,4 +175,17 @@ void free_queue(queue *q)
     }
     q->head = NULL;
     q->tail = NULL;
+}
+
+int count_process_number(queue *q)
+{
+    queue_node *current = q->head;
+    int count = 0;
+
+    while (current != NULL)
+    {
+        count += 1;
+        current = current->next;
+    }
+    return count;
 }
